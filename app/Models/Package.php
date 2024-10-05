e987c70968640f7b5ba4ce88b6b25ebb1b816a98
Temur Komilov <itteamdev74@gmail.com>
2024-10-05 17:29:27 +0500

    packages migration and model created

---
+++ app/Models/Package.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Package extends Model
+{
+    // [
+    //     "id" => 2,
+    //     "price_per_person" => 1230,
+    //     "location" => "Netherlands",
+    //     "rating" => 5,
+    //     "desc" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit luctus nec ullam. Ut elit tellus, luctus nec ullam elit tellpus.",
+    //     "title" => "Experience the natural beauty of island",
+    //     "slug" => "experience-the-natural-beauty-of-island",
+    //     "image" => "/assets/images/img5.jpg"
+    // ],
+    protected $table = 'packages';
+    protected $fillable = [
+        "price_per_person",
+        "location",
+        "rating",
+        "desc",
+        "title",
+        "slug",
+        "image"
+    ];
+    use HasFactory;
+}
